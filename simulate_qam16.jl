5485470ec5db6e5c410ba16999277c071eff8c64
Chris Winstead <chris.winstead@usu.edu>
2025-12-23 10:11:18 -0700

    Updated for more flexible simulations.

--- simulate_qam16.jl
+++ simulate_qam16.jl
@@ -8,21 +8,30 @@ include("src/Experiment.jl")
 m    = QAM(16)
 
 # NBLDPC Code Definition
-code = BlockCode(GF(16),"codes/204.102.3.6.16.csv")
+code     = BlockCode(GF(16),"codes/204.102.3.6.16.csv")
+codename = "204.102.3.6.16"
+
+params = [:SNR,:T,:w,:η,:κ,:λ]
 
 T    = 480
 SNR  = 6.0
-
+w    = 25
+η    = 1.0
+κ    = 5
+λ    = 6
+    
 # Initialize log for NBLDPC code
 experiment = Experiment(code=code,m=m)
-experiment.name="NGDSF_T$(T)"
+experiment.name="NGDSF"
 experiment.description="N=204 NBLDPC over 𝔽₁₆ decoded with NGDSF"
 experiment.notes="""
 Noisy Gradient Descent Symbol Flipping experiment on QAM16 AWGN channel.
 Parameters chosen to replicate Zinnia's Matlab implementation. 
 """
 
-result_filename = joinpath("results","$(experiment.name)_$(experiment.date)_$(experiment.commit).ser")
+result_path = joinpath("results",experiment.name,codename,experiment.commit)
+mkpath(result_path)
+result_filename = joinpath(result_path,"$(experiment.date).ser")
 if isfile(result_filename)
     experiment=deserialize(result_filename)
 end
@@ -38,10 +47,10 @@ macro dec(chan)
             modulation=m,    
             chan=$(esc(chan)),
             T=T,
-            w=25,
-            η=0.3,
-            κ=6,
-            λ=4
+            w=w,
+            η=η,
+            κ=κ,
+            λ=λ
         )
     end        
 end
@@ -66,6 +75,22 @@ function save(e::Experiment)
     serialize(result_filename,e)
 end
 
+
+println("""
+The environment is setup for an experiment named $(experiment.name)
+
+Current settings:
+$(for x in params
+println(string(x),"=",eval(x))
+end)
+
+Change any parameter as desired before running a simulation.
+To sweep a parameter, use runSweep!(experiment,symbol,values)
+
+example: the command below will simulate a range of SNRs
+
+runSweep!(experiment,:SNR,collect(8.0:1.0:13.0))
+""")
 
 #========================================================
  MAIN SIMULATION LOOP
